b8f01869cd6e4c2ffb4f1ac1372455cdb4428e25
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-11-16 12:47:45 +0530

    Added react-slick and slick-carousel dependencies, added card and product component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Product from "./components/Product";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <Product />
       <Footer />
     </>
   );
