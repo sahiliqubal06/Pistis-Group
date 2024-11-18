7361a8f993eb62ad03d5724bfb0429509f399e12
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-11-18 17:38:59 +0530

    Added react-hook-form dependency and implemented form validation in Login component, updated Cards component, and made minor changes to index.css tailwind.config.js

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Cards = ({ item }) => {
-  console.log(item);
   return (
     <>
       <div className="mt-4 my-3 p-3">
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,40 +1,67 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
+      <dialog id="my_modal_3" className="modal ">
+        <div className="modal-box bg-slate-100 dark:bg-slate-900 dark:text-white">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <button
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
             </button>
+            <h3 className="font-bold text-lg">Login</h3>
+            <h1>Only Admins Are Allowed To Access.</h1>
+            <div className="mt-4 space-y-2">
+              <span>Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500">
+                  This field is required.
+                </span>
+              )}
+            </div>
+            <div className="mt-4 space-y-2">
+              <span>Password</span>
+              <br />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  This field is required.
+                </span>
+              )}
+            </div>
+            <div className="mt-6">
+              <button className="bg-blue-500 text-white rounded-md px-3 py-1 hover:bg-blue-700 duration-200">
+                Login
+              </button>
+            </div>
           </form>
-          <h3 className="font-bold text-lg">Login</h3>
-          <h1>Only Admins Are Allowed To Access These Resources</h1>
-
-          <div className="mt-4 space-y-2">
-            <span>Email</span>
-            <br />
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          <div className="mt-4 space-y-2">
-            <span>Password</span>
-            <br />
-            <input
-              type="text"
-              placeholder="Enter your password"
-              className="w-80 px-3 py-1 border rounded-md outline-none"
-            />
-          </div>
-          <div className="mt-4">
-            <button className="px-2 py-1 rounded-full border-[2px] hover:bg-blue-500 hover:text-white">Login</button>
-          </div>
         </div>
       </dialog>
     </div>
